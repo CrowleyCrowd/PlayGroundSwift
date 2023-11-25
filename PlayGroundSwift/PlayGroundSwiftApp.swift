353a9b881beac7b852a01753760e987de305f57a
Efrenn Alba <92108836+CrowleyCrowd@users.noreply.github.com>
2023-11-25 12:42:54 -0500

    Add files via upload

---
+++ PlayGroundSwift/PlayGroundSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayGroundSwiftApp.swift
+//  PlayGroundSwift
+//
+//  Created by Usuario invitado on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayGroundSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
